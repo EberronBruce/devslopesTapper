b7258b88b42d6d5a94e83014b1c0e58e42419bf2
Bruce Burgess <bruceburgess67@yahoo.com>
2016-03-18 15:02:15 +0700

    added hidden and showing

--- tapper-extreme/ViewController.swift
+++ tapper-extreme/ViewController.swift
@@ -11,6 +11,21 @@ import UIKit
 class ViewController: UIViewController {
 
 
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var tapBtn: UIButton!
+    @IBOutlet weak var tapsLbl: UILabel!
+    
+    @IBAction func onPlayBtnPressed(sender: UIButton!) {
+        logoImg.hidden = true
+        playBtn.hidden = true
+        howManyTapsTxt.hidden = true
+        
+        tapBtn.hidden = false
+        tapsLbl.hidden = false
+    }
 
 
 }
